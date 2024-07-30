3c9d053e6fe16d9ce5e4b767b15949d84c0ec30a
m.scarpelli <m.scarpelli@codiceclick.com>
2024-07-30 11:24:35 +0200

    creazione utente modifiche catturato input utente per username attraverso un campo di testo dentro a un popup che diventa visibile al click del buttonCreateUser

--- JBlackJack/src/view/MenuPanel.java
+++ JBlackJack/src/view/MenuPanel.java
@@ -9,26 +9,26 @@ import controller.CreateUserActionListener;
 public class MenuPanel extends JPanel
 {
 	private JButton buttonCreateUser;
-	private JTextField fieldUsername;
 	
 	public MenuPanel()
 	{
 		setLayout(new BorderLayout());
 		
-		fieldUsername = new JTextField();
-		add(fieldUsername, BorderLayout.CENTER);
-		
-		buttonCreateUser = new JButton("+");	
-		
-		//posso usare una lambda, ActionListener è un interfaccai funzionale
+		buttonCreateUser = new JButton("Crea nuovo utente");
+		//posso usare una lambda, ActionListener è un interfaccia funzionale
 		buttonCreateUser.addActionListener(e -> {
-            String username = fieldUsername.getText().trim();
-            if (!username.isEmpty()) {
-                new CreateUserActionListener(username).actionPerformed(e);
-            } else {
-                JOptionPane.showMessageDialog(null, "Inserire un username.", "Errore", JOptionPane.ERROR_MESSAGE);
-            }
-        });
+			String username = JOptionPane.showInputDialog(null, "Inserire un username", "Creazione utente", JOptionPane.PLAIN_MESSAGE);
+			
+			//se clicco su OK
+			if (username != null) {
+				//controlla la validita dell'input (se il campo non è vuoto fai l'actionPerformed del CreateUserActionListener, altrimenti manda un errore)
+				if (!username.isEmpty()) {
+	                new CreateUserActionListener(username).actionPerformed(e);
+	            } else {	
+	                JOptionPane.showMessageDialog(null, "l'username non può essere vuoto!", "Errore", JOptionPane.ERROR_MESSAGE);
+	            }
+	        } 
+		});
 		add(buttonCreateUser,BorderLayout.NORTH);
 	}
 	
